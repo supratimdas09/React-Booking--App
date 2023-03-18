b72ba39c2546e0850f9af9946765c85c8066ac3b
Supratim Das <supratimdas62503@gmail.com>
2023-03-18 20:31:19 +0530

    initial

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="fLists">
+            <ul className="fList">
+                <li className="flistItem">Contries</li>
+                <li className="flistItem">Regions</li>
+                <li className="flistItem">Cities</li>
+                <li className="flistItem">Districts</li>
+                <li className="flistItem">Hotels</li>
+            </ul>
+            <ul className="fList">
+                <li className="flistItem">Contries</li>
+                <li className="flistItem">Regions</li>
+                <li className="flistItem">Cities</li>
+                <li className="flistItem">Districts</li>
+                <li className="flistItem">Hotels</li>
+            </ul>
+            <ul className="fList">
+                <li className="flistItem">Contries</li>
+                <li className="flistItem">Regions</li>
+                <li className="flistItem">Cities</li>
+                <li className="flistItem">Districts</li>
+                <li className="flistItem">Hotels</li>
+            </ul>
+            <ul className="fList">
+                <li className="flistItem">Contries</li>
+                <li className="flistItem">Regions</li>
+                <li className="flistItem">Cities</li>
+                <li className="flistItem">Districts</li>
+                <li className="flistItem">Hotels</li>
+            </ul>
+            <ul className="fList">
+                <li className="flistItem">Contries</li>
+                <li className="flistItem">Regions</li>
+                <li className="flistItem">Cities</li>
+                <li className="flistItem">Districts</li>
+                <li className="flistItem">Hotels</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
